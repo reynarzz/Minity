b0369db532d5a2428f250cf721418532f7fc457a
Reynard_Perez <reynardo19@hotmail.com>
2021-01-13 11:47:10 -0400

    Licence

--- Assets/Editor/MinityViewportEditor.cs
+++ Assets/Editor/MinityViewportEditor.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿//MIT License
+
+//Copyright (c) 2021 Reynardo Perez
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,115 +54,126 @@ namespace MinityEngine
 
             // MinityScene.SetPos(_minity.transform.position.x, _minity.transform.position.y, _minity.transform.position.z);
 
+            var prevRect = GUILayoutUtility.GetLastRect();
+
             rect.width += rect.x - 3;
             rect.x = 3;
-
-            var deltaTime = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f - _totalSecs;
-            _totalSecs = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f;
-
-            MinityScene.SetTime((float)_stopWatch.Elapsed.TotalMilliseconds, (float)deltaTime);
-
-            MinityScene.SetScreenValues(rect.width, 230f, rect.width / 230f);
-            MinityScene.SetMoveSpeed(10f);
-            // Background.
             EditorGUI.DrawRect(rect, Color.black * 0.5f);
 
-            // if (Event.current.type == EventType.Repaint)
+            if (!MinityWindow.IsOpened)
             {
+                var deltaTime = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f - _totalSecs;
+                _totalSecs = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f;
+
+                MinityScene.SetTime((float)_stopWatch.Elapsed.TotalMilliseconds, (float)deltaTime);
+
+                MinityScene.SetScreenValues(rect.width, 230f, rect.width / 230f);
+                MinityScene.SetMoveSpeed(10f);
+                Debug.Log(rect.y);
+
                 GUI.BeginClip(rect);
-                rect.y = rect.height / 2 - 17;
+
                 GL.Viewport(rect);
 
                 GL.IssuePluginEvent(MinityScene.Run(), 0);
 
                 GUI.EndClip();
-            }
 
+            }
+            else
+            {
+                GUI.Label(new Rect(rect.x, rect.y, Screen.width, 20), "Opened in another window");
+            }
             GUILayout.Space(5);
+
             if (GUILayout.Button("Open In Window"))
             {
                 MinityWindow.Open();
             }
 
-            Event current = Event.current;
-
-            MinityScene.SetMouseData(current.mousePosition.x, current.mousePosition.y, current.delta.x, current.delta.y);
-
-            if (current.type == EventType.ScrollWheel)
+            if (!MinityWindow.IsOpened)
             {
+                Event current = Event.current;
 
+                MinityScene.SetMouseData(current.mousePosition.x, current.mousePosition.y, current.delta.x, current.delta.y);
+
+                if (current.type == EventType.ScrollWheel)
+                {
+
+                }
+
+                if (current.alt)
+                {
+                    MinityScene.SetKeyDow(2, 1);
+                }
+                else
+                {
+                    MinityScene.SetKeyUp(2);
+                }
+
+                if (current.isMouse)
+                {
+                    if (current.type == EventType.MouseDown)
+                    {
+                        //Debug.Log("left");
+                        MinityScene.SetKeyDow(1, 1);
+                    }
+
+                    if (current.type == EventType.MouseUp)
+                    {
+                        MinityScene.SetKeyUp(1);
+                    }
+                }
+
+
+                if (current.type == EventType.KeyDown)
+                {
+                    if (current.keyCode == KeyCode.A)
+                    {
+                        MinityScene.SetKeyDow(0, 2);
+                    }
+                    else if (current.keyCode == KeyCode.D)
+                    {
+                        MinityScene.SetKeyDow(0, 4);
+                    }
+
+                    if (current.keyCode == KeyCode.W)
+                    {
+                        MinityScene.SetKeyDow(0, 1);
+                    }
+                    else if (current.keyCode == KeyCode.S)
+                    {
+                        MinityScene.SetKeyDow(0, 3);
+                    }
+
+                    if (current.keyCode == KeyCode.Q)
+                    {
+                        MinityScene.SetKeyDow(0, 5);
+                    }
+                    else if (current.keyCode == KeyCode.E)
+                    {
+                        MinityScene.SetKeyDow(0, 6);
+                    }
+                }
+                else if (current.type == EventType.KeyUp)
+                {
+                    MinityScene.SetKeyUp(0);
+
+                    //if (current.keyCode == KeyCode.A)
+                    //{
+                    //    MinityViewport.SetKeyUp(0);
+
+                    //    Debug.Log("Release A");
+                    //}
+                    //else if (current.keyCode == KeyCode.D)
+                    //{
+                    //    MinityViewport.SetKeyUp(0);
+
+                    //    Debug.Log("Release D");
+                    //}
+                }
             }
 
-            if (current.alt)
-            {
-                MinityScene.SetKeyDow(2, 1);
-            }
-            else
-            {
-                MinityScene.SetKeyUp(2);
-            }
-
-            if (current.isMouse)
-            {
-                if (current.type == EventType.MouseDown)
-                {
-                    //Debug.Log("left");
-                    MinityScene.SetKeyDow(1, 1);
-                }
-
-                if (current.type == EventType.MouseUp)
-                {
-                    MinityScene.SetKeyUp(1);
-                }
-            }
-
-
-            if (current.type == EventType.KeyDown)
-            {
-                if (current.keyCode == KeyCode.A)
-                {
-                    MinityScene.SetKeyDow(0, 2);
-                }
-                else if (current.keyCode == KeyCode.D)
-                {
-                    MinityScene.SetKeyDow(0, 4);
-                }
-
-                if (current.keyCode == KeyCode.W)
-                {
-                    MinityScene.SetKeyDow(0, 1);
-                }
-                else if (current.keyCode == KeyCode.S)
-                {
-                    MinityScene.SetKeyDow(0, 3);
-                }
-
-                if (current.keyCode == KeyCode.Q)
-                {
-                    MinityScene.SetKeyDow(0, 5);
-                }
-                else if (current.keyCode == KeyCode.E)
-                {
-                    MinityScene.SetKeyDow(0, 6);
-                }
-            }
-            else if (current.type == EventType.KeyUp)
-            {
-                MinityScene.SetKeyUp(0);
-
-                //if (current.keyCode == KeyCode.A)
-                //{
-                //    MinityViewport.SetKeyUp(0);
-
-                //    Debug.Log("Release A");
-                //}
-                //else if (current.keyCode == KeyCode.D)
-                //{
-                //    MinityViewport.SetKeyUp(0);
-
-                //    Debug.Log("Release D");
-                //}
-            }
 
             Repaint();
         }
--- Assets/Editor/Window/MinityWindow.cs
+++ Assets/Editor/Window/MinityWindow.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿//MIT License
+
+//Copyright (c) 2021 Reynardo Perez
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +34,26 @@ namespace MinityEngine
     public class MinityWindow : EditorWindow
     {
         private static EditorWindow _window;
+        private System.Diagnostics.Stopwatch _stopWatch;
+        private double _totalSecs;
+
+        private static bool _isOpened;
+        public static bool IsOpened => _isOpened;
 
         public static void Open()
         {
             _window = GetWindow<MinityWindow>("Minity Editor");
+            _isOpened = true;
+        }
+
+        private void OnEnable()
+        {
+            _isOpened = true;
+        }
+
+        private void OnDestroy()
+        {
+            _isOpened = false;
         }
 
         private void Update()
@@ -25,90 +63,128 @@ namespace MinityEngine
 
         private void OnGUI()
         {
-            MinityScene.SetScreenValues(Screen.width, Screen.height, (float)Screen.width / Screen.height);
+            if (_stopWatch == null) 
+            {
+                _stopWatch = new System.Diagnostics.Stopwatch();
+                _stopWatch.Start();
+                _totalSecs = _stopWatch.Elapsed.TotalSeconds / 1000.0f;
+            }
 
-            GL.IssuePluginEvent(MinityScene.Run(), 0);
+            Rect rect = GUILayoutUtility.GetRect(Screen.width, Screen.height);
+            EditorGUI.DrawRect(rect, Color.black * 0.5f);
 
-            //Event current = Event.current;
 
-            ////MinityViewport.SetMouseData(current.mousePosition.x, current.mousePosition.y, current.delta.x / 12, current.delta.y / 12);
-            
-            //if (current.type == EventType.ScrollWheel)
-            //{
+            rect.width += rect.x - 3;
+            rect.x = 3;
 
-            //}
+            var deltaTime = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f - _totalSecs;
+            _totalSecs = _stopWatch.Elapsed.TotalMilliseconds / 1000.0f;
 
-            //if (current.alt)
-            //{
-            //    MinityScene.SetKeyDow(2, 1);
-            //}
-            //else
-            //{
-            //    MinityScene.SetKeyUp(2);
-            //}
+            MinityScene.SetTime((float)_stopWatch.Elapsed.TotalMilliseconds, (float)deltaTime);
 
-            //if (current.isMouse) 
-            //{
-            //    if (current.type == EventType.MouseDown)
-            //    {
-            //        //Debug.Log("left");
-            //        MinityScene.SetKeyDow(1, 1);
-            //    }
+            MinityScene.SetScreenValues(rect.width, Screen.height, rect.width / Screen.height);
+            MinityScene.SetMoveSpeed(10f);
 
-            //    if (current.type == EventType.MouseUp)
-            //    {
-            //        MinityScene.SetKeyUp(1);
-            //    }
-            //}
 
-            //if (current.type == EventType.KeyDown)
-            //{
-                
-            //    if (current.keyCode == KeyCode.A)
-            //    {
-            //        MinityScene.SetKeyDow(0, 2);
-            //    }
-            //    else if (current.keyCode == KeyCode.D) 
-            //    {
-            //        MinityScene.SetKeyDow(0, 4);
-            //    }
+            // Background.
 
-            //    if (current.keyCode == KeyCode.W)
-            //    {
-            //        MinityScene.SetKeyDow(0, 1);
-            //    }
-            //    else if (current.keyCode == KeyCode.S)
-            //    {
-            //        MinityScene.SetKeyDow(0, 3);
-            //    }
+            // if (Event.current.type == EventType.Repaint)
+            {
+                GUI.BeginClip(rect);
+                //rect.y = rect.height /;
+                rect.y -= 20;
+                GL.Viewport(rect);
 
-            //    if (current.keyCode == KeyCode.Q)
-            //    {
-            //        MinityScene.SetKeyDow(0, 5);
-            //    }
-            //    else if (current.keyCode == KeyCode.E)
-            //    {
-            //        MinityScene.SetKeyDow(0, 6);
-            //    }
+                GL.IssuePluginEvent(MinityScene.Run(), 0);
 
-            //}
-            //else if (current.type == EventType.KeyUp)
-            //{
-            //        MinityScene.SetKeyUp(0);
+                GUI.EndClip();
+            }
 
-            //    //if (current.keyCode == KeyCode.A)
-            //    //{
-            //    //    MinityViewport.SetKeyUp(0);
+            GUILayout.Space(5);
+            if (GUILayout.Button("Open In Window"))
+            {
+                MinityWindow.Open();
+            }
 
-            //    //    Debug.Log("Release A");
-            //    //}
-            //    //else if (current.keyCode == KeyCode.D)
-            //    //{
-            //    //    MinityViewport.SetKeyUp(0);
+            Event current = Event.current;
 
-            //    //    Debug.Log("Release D");
-            //    //}
-            //}
+            MinityScene.SetMouseData(current.mousePosition.x, current.mousePosition.y, current.delta.x, current.delta.y);
+
+            if (current.type == EventType.ScrollWheel)
+            {
+
+            }
+
+            if (current.alt)
+            {
+                MinityScene.SetKeyDow(2, 1);
+            }
+            else
+            {
+                MinityScene.SetKeyUp(2);
+            }
+
+            if (current.isMouse)
+            {
+                if (current.type == EventType.MouseDown)
+                {
+                    //Debug.Log("left");
+                    MinityScene.SetKeyDow(1, 1);
+                }
+
+                if (current.type == EventType.MouseUp)
+                {
+                    MinityScene.SetKeyUp(1);
+                }
+            }
+
+
+            if (current.type == EventType.KeyDown)
+            {
+                if (current.keyCode == KeyCode.A)
+                {
+                    MinityScene.SetKeyDow(0, 2);
+                }
+                else if (current.keyCode == KeyCode.D)
+                {
+                    MinityScene.SetKeyDow(0, 4);
+                }
+
+                if (current.keyCode == KeyCode.W)
+                {
+                    MinityScene.SetKeyDow(0, 1);
+                }
+                else if (current.keyCode == KeyCode.S)
+                {
+                    MinityScene.SetKeyDow(0, 3);
+                }
+
+                if (current.keyCode == KeyCode.Q)
+                {
+                    MinityScene.SetKeyDow(0, 5);
+                }
+                else if (current.keyCode == KeyCode.E)
+                {
+                    MinityScene.SetKeyDow(0, 6);
+                }
+            }
+            else if (current.type == EventType.KeyUp)
+            {
+                MinityScene.SetKeyUp(0);
+
+                //if (current.keyCode == KeyCode.A)
+                //{
+                //    MinityViewport.SetKeyUp(0);
+
+                //    Debug.Log("Release A");
+                //}
+                //else if (current.keyCode == KeyCode.D)
+                //{
+                //    MinityViewport.SetKeyUp(0);
+
+                //    Debug.Log("Release D");
+                //}
+            }
 
             Repaint();
         }
